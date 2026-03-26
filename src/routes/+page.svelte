75e031f85d4645c2c907a45058d542434e7f3acc
PanuwatRodtuadee <bhlg1644.bike@gmail.com>
2026-03-27 03:24:23 +0700

    add email and password box

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,16 @@
 <header>
     <h1>Demo Honey Pot</h1>
 </header>
+<body>
+    <main class = "login">
+    <h2>เข้าสู่ระบบ</h2>
+    <div class = "email">
+        <label for="email">email : </label>
+        <input type="email" id = "email" name = "email" required placeholder="example@email.com">
+    </div> 
+    <div class = "password">
+        <label for="password">password : </label>
+        <input type="password" name="password" id="password">
+    </div>
+    </main>
+</body>
